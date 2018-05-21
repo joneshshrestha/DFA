324725b07f05625d13521aa5fa2cc5bd7b11f726
Jonesh Shrestha <joneshshrestha69@gmail.com>
2018-05-21 16:54:00 +0545

    string starting with 1 and ending with 0

---
+++ start_1_end_0.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+//DFA that accepts strings starting with 1 and ending with 0
+int main() {
+    int i;
+    char str[MAX], f = 'A', response;
+    do {
+        printf("Enter string (0 or 1): ");
+        scanf("%s", str);
+        for (i = 0; i < strlen(str); i++) {
+            switch(f)
+            {
+                case 'A':
+                    if(str[i] == '0') f = 'D';
+                    else if(str[i] == '1') f = 'B';
+                    break;
+                    
+                case 'B':
+                    if(str[i] == '0') f = 'C';
+                    else if(str[i] == '1') f = 'B';
+                    break;
+                    
+                case 'C':
+                    if(str[i] == '0') f = 'C';
+                    else if(str[i] == '1') f = 'B';
+                    break;
+                    
+                case 'D':
+                    if(str[i] == '0') f = 'D';
+                    else if(str[i] == '1') f = 'D';
+                    break;
+            }
+        }
+        if (f == 'C') printf("Accepted \n");
+        else printf("Rejected \n");
+        printf("Do you want to continue(y/n): ");
+        scanf("%s", &response);
+    } while(response == 'y');
+}
+
+
+
+
+
